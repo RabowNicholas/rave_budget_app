e609b0af64eb48871aea1b68b7beb787bb30d083
RabowNicholas <rabownick123@gmail.com>
2024-09-26 16:08:22 -0600

    import error fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { DashboardData } from "./model";
 
 export default function Dashboard() {
   const [data, setData] = useState<DashboardData | undefined>();
