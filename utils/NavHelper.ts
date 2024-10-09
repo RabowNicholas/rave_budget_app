c45f5b5499af2b34e593e330f8f7ba91223a1e2b
RabowNicholas <rabownick123@gmail.com>
2024-10-09 13:50:33 -0600

    update app base url helper

--- utils/NavHelper.ts
+++ utils/NavHelper.ts
@@ -1,3 +1,8 @@
 export function getAppBaseURL() {
-  return "http://localhost:3000";
+  const env = process.env.NEXT_PUBLIC_ENVIRONMENT;
+  if (env === "local") {
+    return "http://localhost:3000";
+  } else {
+    return "https://fest-fund.com";
+  }
 }
