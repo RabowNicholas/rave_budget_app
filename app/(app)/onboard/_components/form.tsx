e2281b6f86369f8cea43e9fa15132eef158f99c3
RabowNicholas <rabownick123@gmail.com>
2024-10-29 11:28:11 -0600

    ui styling large screens

--- app/(app)/onboard/_components/form.tsx
+++ app/(app)/onboard/_components/form.tsx
@@ -38,27 +38,32 @@ export default function OnboardForm() {
     }
   };
   return (
-    <form className="h-dvh flex flex-col gap-6 w-full" onSubmit={handleSubmit}>
-      <div className="text-2xl font-bold bg-gradient-to-r from-[#A100FF] via-[#FFD700] to-[#00A676] w-fit bg-clip-text text-transparent mb-4 uppercase">
-        Onboarding
-      </div>
+    <div className="flex flex-col w-full items-center justify-center h-dvh ">
+      <form
+        className="flex flex-col gap-6 w-full text-white max-w-[400px] lg:border lg:border-3 lg:border-lightBackground lg:rounded-md lg:px-8 lg:py-6"
+        onSubmit={handleSubmit}
+      >
+        <div className="text-2xl font-bold bg-gradient-to-r from-[#A100FF] via-[#FFD700] to-[#00A676] w-fit bg-clip-text text-transparent mb-4 uppercase">
+          Onboarding
+        </div>
 
-      <div className="flex flex-col gap-2">
-        <label className="ml-3 text-lg " htmlFor="name">
-          Name{" "}
-        </label>
-        <input
-          className="px-4 py-2 rounded-lg border-2 border-transparent bg-mediumGray text-white focus:outline-none focus:ring-2 focus:ring-vibrantPurple placeholder-white"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-        />
-      </div>
-      {error && <p className="text-red-500">{error}</p>}
-      <button className="button-primary-filled" type="submit">
-        Submit
-      </button>
-    </form>
+        <div className="flex flex-col gap-2">
+          <label className="ml-3 text-lg " htmlFor="name">
+            Name{" "}
+          </label>
+          <input
+            className="px-4 py-2 rounded-lg border-2 border-transparent bg-mediumGray text-white focus:outline-none focus:ring-2 focus:ring-vibrantPurple placeholder-white"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+        </div>
+        {error && <p className="text-red-500">{error}</p>}
+        <button className="button-primary-filled" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
